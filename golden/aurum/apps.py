5854fa6ec623783f1af2d4f0040ef445454a24ba
miss-aurum <turdubekaltynkyz@gmail.com>
2023-02-28 20:18:16 +0600

    l

---
+++ golden/aurum/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AurumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aurum'
---
+++ golden/aurum/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, template_name='aurum/index.html')
+
+
+
+
+
+
